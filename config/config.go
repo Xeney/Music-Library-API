49cfb10c2523e9d48859213c5876c8e12db06f25
Xeney <jeneksero@gmail.com>
2025-03-26 17:16:57 +0400

    .

---
+++ config/config.go
@@ -0,0 +1,19 @@
+package config
+
+import "github.com/caarlos0/env"
+
+type Config struct {
+	DBHost     string `env:"DB_HOST"`
+	DBPort     string `env:"DB_PORT"`
+	DBUser     string `env:"DB_USER"`
+	DBPassword string `env:"DB_PASSWORD"`
+	DBName     string `env:"DB_NAME"`
+}
+
+func Load() (*Config, error) {
+	cfg := &Config{}
+	if err := env.Parse(cfg); err != nil {
+		return nil, err
+	}
+	return cfg, nil
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module music-library-api
+
+go 1.24.1
+
+require github.com/caarlos0/env v3.5.0+incompatible // indirect
